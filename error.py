283f01171b50e56bc40b0a4728a4a1ce434087c4
malikrehanmulla1048 <malikrehanmulla107@gmail.com>
2025-11-30 22:39:02 +0530

    the new changes are added

--- error.py
+++ error.py
@@ -1,11 +1,14 @@
 # def spam():
-#     print(eggs) /"here it gave the output as error"/
+#     print(eggs) /"here it gave the output as error"/?//name error//?
 #     eggs="spam local"
-eggs="global spam"
+# eggs="global spam"
 # spam()
 def spam():
-    # print(eggs)
     eggs="spam local"
+    bacon()
+    print(eggs)
+def bacon():
+    eggs="bacon local"
+    shell="white"
     print(eggs)
 spam()
-print(eggs)
